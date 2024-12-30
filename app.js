c932e2b6345fc031c6b49ef293546765a3851491
Aditya Jain <adityajbp92@gmail.com>
2024-12-31 01:50:47 +0530

    updated HTML , JS

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-// Select button element using querySelector()
-var btntrn = document.querySelector("#click-action");
+// // Select button element using querySelector()
+// var btntrn = document.querySelector("#click-action");
 
-var txtinput = document.querySelector("#txt-input");
+// var txtinput = document.querySelector("#txt-input");
 
-var outputdiv = document.querySelector("#output");
+// var outputdiv = document.querySelector("#output");
 
-function clickevent() {
-      outputdiv.innerText ="aeieee  " + txtinput.value;
-};
+// function clickevent() {
+//       outputdiv.innerText ="aeieee  " + txtinput.value;
+// };
 
-// what will happen everytime User click the button , so we use addEventListener 
-btntrn.addEventListener("click", clickevent);
+// // what will happen everytime User click the button , so we use addEventListener 
+// btntrn.addEventListener("click", clickevent);
+
+
+var btntrn=document.querySelector("#click-action");
+var txtinput=document.querySelector("#txt-input");
+var outputdiv=document.querySelector("#output");
+
+function transURL(text){
+      return "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + text ;
+}
+
+function clickhandler(){
+      var inputtext=txtinput.value ;
+      fetch(transURL(text)) {
+            .then(response => response.json);
+            .then(json => console.log(json));
+      }
+
+}
+
+btntrn.addEventListener("click",clickhandler)
