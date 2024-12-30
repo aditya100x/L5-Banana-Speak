79c47ee7b71006b26a4233cb6c313714ef32db91
Aditya Jain <adityajbp92@gmail.com>
2024-12-31 02:04:39 +0530

    Done HTML , JS

--- app.js
+++ app.js
@@ -16,18 +16,27 @@
 var btntrn=document.querySelector("#click-action");
 var txtinput=document.querySelector("#txt-input");
 var outputdiv=document.querySelector("#output");
+var serverurl="https://api.funtranslations.com/translate/minion.json";
 
 function transURL(text){
-      return "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + text ;
+      return serverurl + "?" + "text=" + text ;
+}
+function errorhandler(error){
+      console.log("some error occured ", error);
+      alert("Something went wrong, Server is down");
+
 }
 
 function clickhandler(){
       var inputtext=txtinput.value ;
-      fetch(transURL(text)) {
-            .then(response => response.json);
-            .then(json => console.log(json));
-      }
+      fetch(transURL(inputtext)) 
+            .then(response => response.json())
+            .then(json => {
+                  var translatedtext=json.contents.translated;
+                  outputdiv.innerText=translatedtext;
+            })
+            .catch(errorhandler);
+      
+};
 
-}
-
-btntrn.addEventListener("click",clickhandler)
+btntrn.addEventListener("click",clickhandler);
